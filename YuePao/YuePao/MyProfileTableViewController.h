0d4a3519a442324ff87d218851ab1716a2e3fa4e
suankuang <suankuang@163.com>
2015-07-18 00:52:45 +0800

    add class MyProfileTableViewController, which is responsible for updating my info, and showing friends and activities related to me.

---
+++ YuePao/YuePao/MyProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyProfileTableViewController.h
+//  YuePao
+//
+//  Created by lime on 15/7/18.
+//  Copyright (c) 2015年 weilaizhineng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyProfileTableViewController : UITableViewController
+
+@end
